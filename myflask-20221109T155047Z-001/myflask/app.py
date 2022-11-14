e5a9884b7958bd87ff50b052f525ed9a70640da7
Bowiya <99666244+Bowiya@users.noreply.github.com>
2022-11-14 15:58:10 +0530

    Add files via upload

---
+++ myflask-20221109T155047Z-001/myflask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+
+import ibm_db
+conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=21fecfd8-47b7-4937-840d-d791d0218660.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31864;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=mvl42239;PWD=ckKJiFkmUjdUN3Wj;",'','')
+print('connection succesful')
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/forgot")
+def forgot():
+    return render_template('forgot.html')
+
+@app.route("/dashbord")
+def about():
+    return render_template('dashboard.html')
